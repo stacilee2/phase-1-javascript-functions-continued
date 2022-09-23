fe0ba7467e0a3e7eee4c7024728bc0aeba986c25
Staci Rosales <stacileep2@gmail.com>
2022-09-23 14:18:10 -0500

    commit tree f4985d0f1d2e3da4cab640161fef14664f1b93ef

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(activity= 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+function wrapAdjective(flair = '*') {
+    return function(adjective = 'hard') {
+        return `You are ${flair}${adjective}${flair}!`
+    };
+};
+wrapAdjective();
+
